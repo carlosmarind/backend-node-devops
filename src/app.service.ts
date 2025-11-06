f16fa6e1984e086d62f09d30b1aa8fbc40c6703e
Carlos Marin Diaz <carlosmarind@gmail.com>
2025-11-05 21:38:11 -0300

    actualizacion de aplicacion

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!!';
+    return 'Saludos desde mi CICD';
   }
 
   getHola(): string {
